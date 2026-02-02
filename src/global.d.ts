32b8be9cbab36faa62218c38196f1144bf2f75f9
cocobo1 <CocoboTheOne@gmail.com>
2026-02-02 19:55:52 +0200

    feat: unfuck ios support

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,6 +1,5 @@
 declare global {
         type React = typeof import("react");
-        var SkiaApi: typeof import("@shopify/react-native-skia").Skia;
 
         // ReactNative/Hermes globals
         var globalEvalWithSourceUrl: (script: string, sourceURL: string) => any;
