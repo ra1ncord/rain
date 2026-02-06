a6c4f4020b9c70b520fd881dd7b936532aea16c7
j <joamonca@joamonca.tech>
2026-02-06 17:26:28 +0100

    fix(fakenitro): make settings scrollable

--- src/rain/plugins/fakenitro/settings.tsx
+++ src/rain/plugins/fakenitro/settings.tsx
@@ -1,90 +1,93 @@
-import { findByProps } from "@metro";
-import { fakenitroSettings } from "./storage";
-import { ReactNative as RN } from "@metro/common";
+import {findByProps} from "@metro";
+import {fakenitroSettings} from "./storage";
+import {ReactNative as RN} from "@metro/common";
+import {ScrollView} from "react-native";
 
 const {
-	TableRow,
-	TableSwitchRow,
-	TableRadioGroup,
-	TableRadioRow,
-	TableRowGroup,
+    TableRow,
+    TableSwitchRow,
+    TableRadioGroup,
+    TableRadioRow,
+    TableRowGroup,
 } = findByProps("TableRow");
-const { Stack } = findByProps("Stack");
+const {Stack} = findByProps("Stack");
 
 const sizeOptions = {
-	Tiny: 16,
-	Small: 32,
-	Medium: 48,
-	Large: 64,
-	Huge: 96,
-	Jumbo: 128,
+    Tiny: 16,
+    Small: 32,
+    Medium: 48,
+    Large: 64,
+    Huge: 96,
+    Jumbo: 128,
 };
 
 const previewUri = "https://cdn.discordapp.com/emojis/926602689213767680.webp";
 
 export default () => {
-	return (
-		<Stack
-			style={{ paddingVertical: 24, paddingHorizontal: 12 }}
-			spacing={24}
-		>
-			<TableRadioGroup
-				title="Emoji size"
-				defaultValue={fakenitroSettings.emojiSize.toString()}
-				onChange={(v: string) =>
-					(fakenitroSettings.emojiSize = parseInt(v))
-				}
-			>
-				{Object.entries(sizeOptions).map(([name, size]) => (
-					<TableRadioRow
-						label={name}
-						subLabel={size}
-						key={size.toString()}
-						value={size.toString()}
-					/>
-				))}
-			</TableRadioGroup>
-			<TableRowGroup title="Preview">
-				<RN.Image
-					source={{
-						uri: `${previewUri}?size=${fakenitroSettings.emojiSize}`,
-						width: fakenitroSettings.emojiSize,
-						height: fakenitroSettings.emojiSize,
-					}}
-				/>
-			</TableRowGroup>
-			<TableRowGroup title="User Profile" titleStyleType="no_border">
-				<TableSwitchRow
-					label="Use hyperlinks when sending fake emojis"
-					onValueChange={(v: boolean) => {
-						fakenitroSettings.hyperLink = v;
-					}}
-					value={fakenitroSettings.hyperLink}
-				/>
-				<TableSwitchRow
-					label="Transform fake emojis into real ones"
-					onValueChange={(v: boolean) => {
-						fakenitroSettings.transformEmoji = v;
-					}}
-					value={fakenitroSettings.transformEmoji}
-				/>
-			</TableRowGroup>
-			<TableRowGroup title="Stickers" titleStyleType="no_border">
-				<TableSwitchRow
-					label="Use hyperlinks when sending fake stickers"
-					onValueChange={(v: boolean) => {
-						fakenitroSettings.stickerHyperLink = v;
-					}}
-					value={fakenitroSettings.stickerHyperLink}
-				/>
-				<TableSwitchRow
-					label="Transform fake stickers into real ones"
-					onValueChange={(v: boolean) => {
-						fakenitroSettings.transformSticker = v;
-					}}
-					value={fakenitroSettings.transformSticker}
-				/>
-			</TableRowGroup>
-		</Stack>
-	);
+    return (
+        <ScrollView style={{flex: 1}}>
+            <Stack
+                style={{paddingVertical: 24, paddingHorizontal: 12}}
+                spacing={24}
+            >
+                <TableRadioGroup
+                    title="Emoji size"
+                    defaultValue={fakenitroSettings.emojiSize.toString()}
+                    onChange={(v: string) =>
+                        (fakenitroSettings.emojiSize = parseInt(v))
+                    }
+                >
+                    {Object.entries(sizeOptions).map(([name, size]) => (
+                        <TableRadioRow
+                            label={name}
+                            subLabel={size}
+                            key={size.toString()}
+                            value={size.toString()}
+                        />
+                    ))}
+                </TableRadioGroup>
+                <TableRowGroup title="Preview">
+                    <RN.Image
+                        source={{
+                            uri: `${previewUri}?size=${fakenitroSettings.emojiSize}`,
+                            width: fakenitroSettings.emojiSize,
+                            height: fakenitroSettings.emojiSize,
+                        }}
+                    />
+                </TableRowGroup>
+                <TableRowGroup title="User Profile" titleStyleType="no_border">
+                    <TableSwitchRow
+                        label="Use hyperlinks when sending fake emojis"
+                        onValueChange={(v: boolean) => {
+                            fakenitroSettings.hyperLink = v;
+                        }}
+                        value={fakenitroSettings.hyperLink}
+                    />
+                    <TableSwitchRow
+                        label="Transform fake emojis into real ones"
+                        onValueChange={(v: boolean) => {
+                            fakenitroSettings.transformEmoji = v;
+                        }}
+                        value={fakenitroSettings.transformEmoji}
+                    />
+                </TableRowGroup>
+                <TableRowGroup title="Stickers" titleStyleType="no_border">
+                    <TableSwitchRow
+                        label="Use hyperlinks when sending fake stickers"
+                        onValueChange={(v: boolean) => {
+                            fakenitroSettings.stickerHyperLink = v;
+                        }}
+                        value={fakenitroSettings.stickerHyperLink}
+                    />
+                    <TableSwitchRow
+                        label="Transform fake stickers into real ones"
+                        onValueChange={(v: boolean) => {
+                            fakenitroSettings.transformSticker = v;
+                        }}
+                        value={fakenitroSettings.transformSticker}
+                    />
+                </TableRowGroup>
+            </Stack>
+        </ScrollView>
+    );
 };
