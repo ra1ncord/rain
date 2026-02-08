7bba62b705e84f3678a0330f19cfae261d72770b
cocobo1 <CocoboTheOne@gmail.com>
2026-02-08 10:56:17 +0200

    fix: correct realmoji credit

--- src/rain/plugins/realmoji/index.ts
+++ src/rain/plugins/realmoji/index.ts
@@ -7,7 +7,7 @@ const patches: any[] = [];
 export default definePlugin({
 	name: "Realmoji",
 	description: "Shows FakeNitro emojis and stickers as real ones",
-	author: [{ name: "j", id: 1356632712861192242n }],
+	author: [{ name: "redstonekasi", id: 265064055490871297n }, { name: "rico040", id: 619474349845643275n }, { name: "cocobo1", id: 767650984175992833n }, { name: "j", id: 1356632712861192242n }],
 	id: "realmoji",
 	version: "v1.0.0",
 	start() {
