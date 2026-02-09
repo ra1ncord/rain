fcfb57acd0f485de1e161373040f8c7ce5d3894c
j <joamonca@joamonca.tech>
2026-02-09 17:33:56 +0100

    feat(viewraw): make raw page pretty

--- src/rain/plugins/viewraw/patches/RawPage.tsx
+++ src/rain/plugins/viewraw/patches/RawPage.tsx
@@ -3,38 +3,38 @@ import { showToast } from "@api/ui/toasts"
 import { Codeblock } from "@api/ui/components"
 import { cleanMessage } from "./cleanmessage"
 import {findAssetId} from "@api/assets";
-import {Button} from "@metro/common/components";
+import {Button, Stack, TableRow, TableRowGroup} from "@metro/common/components";
 
 const { ScrollView } = ReactNative
 
 export default function RawPage({ message }: any) {
     const stringMessage = React.useMemo(() => JSON.stringify(cleanMessage(message), null, 4), [message.id])
 
-    const style = { marginBottom: 8 }
-
     return (<>
-        <ScrollView style={{ flex: 1, marginHorizontal: 13, marginVertical: 10 }}>
-            <Button
-                style={style}
-                text="Copy Raw Content"
-                size="small"
-                disabled={!message.content}
-                onPress={() => {
-                    clipboard.setString(message.content)
-                    showToast("Copied content to clipboard", findAssetId("CopyIcon"))
-                }}
-            />
-            <Button
-                text="Copy Raw Data"
-                style={style}
-                size="small"
-                onPress={() => {
-                    clipboard.setString(stringMessage)
-                    showToast("Copied data to clipboard", findAssetId("CopyIcon"))
-                }}
-            />
-            {message.content && <Codeblock selectable style={style}>{message.content}</Codeblock>}
-            <Codeblock selectable>{stringMessage}</Codeblock>
+        <ScrollView style={{ flex: 1, marginVertical: 10 }}>
+            <Stack style={{ paddingVertical: 24, paddingHorizontal: 12 }}>
+                <TableRowGroup title="Actions">
+                <TableRow
+                    label="Copy Raw Content"
+                    icon={<TableRow.Icon source={findAssetId("ClipboardListIcon")}/>}
+                    disabled={!message.content}
+                    onPress={() => {
+                        clipboard.setString(message.content)
+                        showToast("Copied content to clipboard", findAssetId("ClipboardCheckIcon"))
+                    }}
+                />
+                <TableRow
+                    label="Copy Raw Data"
+                    icon={<TableRow.Icon source={findAssetId("ClipboardListIcon")}/>}
+                    onPress={() => {
+                        clipboard.setString(stringMessage)
+                        showToast("Copied data to clipboard", findAssetId("ClipboardCheckIcon"))
+                    }}
+                />
+                </TableRowGroup>
+                {message.content && <Codeblock selectable>{message.content}</Codeblock>}
+                <Codeblock selectable>{stringMessage}</Codeblock>
+            </Stack>
         </ScrollView>
     </>)
 }
