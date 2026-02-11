4c7d00a6923fe3a313952424cdbb0a4e85de1bef
Reyyancli <reyyan19808@gmail.com>
2026-02-11 19:21:16 +0600

    feat(picturelinks): almost working plugin port

---
+++ src/rain/plugins/picturelinks/index.ts
@@ -0,0 +1,20 @@
+import { definePlugin } from "@plugins";
+import picturelinks from "@plugins/picturelinks/patches/picturelinks"
+
+const patches: any[] = []
+
+export default definePlugin({
+    name: "Picture Links",
+    description: "Allows you to click on profile pictures and banners.",
+    author: [{name: "redstonekasi" , id: 265064055490871297n }, { name: "Rico040", id: 619474349845643275n } ],
+    id: "picturelinks",
+    version: "v1.0.0",
+    start() {
+        patches.push(picturelinks)
+    },
+    stop() {
+        console.log("patch value:", patches);
+        for (const unpatch of patches) unpatch()
+            
+    },
+})
