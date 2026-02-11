5cb1d028ed7d800f2e212441486ce6ae34c5246c
cocobo1 <CocoboTheOne@gmail.com>
2026-02-11 17:12:13 +0200

    fix: seperate Bluetoothaudiofix from rainenhancements

---
+++ src/rain/plugins/bluetoothaudiofix/index.ts
@@ -0,0 +1,31 @@
+import { instead } from "@api/patcher";
+import { ReactNative as RN } from "@metro/common";
+import { Platform } from "react-native";
+import { definePlugin, usePluginSettings } from "@plugins";
+import { showToast } from "@api/ui/toasts";
+
+const patches: any[] = [];
+
+export default definePlugin({
+    name: "BluetoothAudioFix",
+    description: "Prevents Discord from enabling handsfree mode while in a call",
+    author: [
+        { name: "Narwhal", id: 455429792871874581n },
+        { name: "redstonekasi", id: 265064055490871297n }
+    ],
+    id: "bluetoothaudiofix",
+    version: "v1.0.0",
+    start() {
+        if (Platform.OS === "ios") { 
+            showToast("This plugin does not do anything on iOS");
+            usePluginSettings.getState().updatePluginSetting("bluetoothaudiofix", false);
+            return; 
+        }
+
+        const onUnload = RN.TurboModuleRegistry.get("NativeAudioManagerModule") === null ? RN.TurboModuleRegistry.get("RTNAudioManager") : RN.TurboModuleRegistry.get("NativeAudioManagerModule");
+        patches.push(instead("setCommunicationModeOn", onUnload, () => {}));
+    },
+    stop() {
+        for (const unpatch of patches) unpatch();
+    },
+});
