e78e149ad14e194df42374667812b210ca97a9e9
kmmiio99o <szymonladziak00@gmail.com>
2026-02-18 21:46:55 +0100

    feat: make Asset Browser core part of the developer settings

--- src/rain/plugins/assetsbrowser/index.tsx
+++
@@ -1,9 +0,0 @@
-import { definePlugin } from "@plugins";
-
-export default definePlugin({
-    name: "AssetBrowser",
-    description: "Browse Discord assets",
-    author: [{ name: "kmmiio99o", id: 879393496627306587n }],
-    id: "assetbrowser",
-    version: "v1.0.0",
-});
