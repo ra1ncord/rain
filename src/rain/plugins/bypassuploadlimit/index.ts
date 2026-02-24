8da158925431ad2d71c228befa9f71b438599541
LampDelivery <lampdelivery@noreply.codeberg.org>
2026-02-24 10:04:50 +0100

    feat: bypassuploadlimit

---
+++ src/rain/plugins/bypassuploadlimit/index.ts
@@ -0,0 +1,22 @@
+import { definePlugin } from "@plugins";
+import { Developers } from "@rain/Developers";
+import getUploaderPatch from "./patches/upload";
+import settings from "./settings";
+
+const patches: (() => boolean)[] = [];
+
+export default definePlugin({
+    name: "BypassUploadLimit",
+    description: "Bypass Discord's file size limit by uploading to Catbox, Litterbox, or Pomf",
+    author: [Developers.LampDelivery],
+    id: "bypassuploadlimit",
+    version: "1.0.0",
+    start() {
+        patches.push(...getUploaderPatch());
+    },
+    stop() {
+        for (const unpatch of patches) unpatch();
+        patches.length = 0;
+    },
+    settings,
+});
