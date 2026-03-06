207178aa994f06977f58cf7cc8d885d68609cc51
cocobo1 <CocoboTheOne@gmail.com>
2026-03-06 18:48:19 +0200

    chore: cleanup a few spec 3 errors

--- src/plugins/_core/painter/themes/types.ts
+++ src/plugins/_core/painter/themes/types.ts
@@ -33,6 +33,11 @@ export interface ThemeManifest {
         blur?: number;
         alpha?: number;
     };
+    display: {
+        name: string;
+        description: string;
+        authors: Author[];
+    }
 }
 
 /** @internal */
