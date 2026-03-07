bd96478728169edcabefeb5b0f8b9d36ed1de6a9
kmmiio99o <szymonladziak00@gmail.com>
2026-03-08 00:43:53 +0100

    fix: settings page not being updated immediately

--- src/plugins/hideblockedandignoredmessages/settings.tsx
+++ src/plugins/hideblockedandignoredmessages/settings.tsx
@@ -1,9 +1,11 @@
 import { Stack, TableRowGroup, TableSwitchRow } from "@metro/common/components";
 import { ScrollView, View } from "react-native";
 
-import { hideblockedandignoredmessagesSettings as settings } from "./storage";
+import { useHideBlockedAndIgnoredMessagesSettings } from "./storage";
 
 export default function Settings() {
+    const settings = useHideBlockedAndIgnoredMessagesSettings();
+
     return (
         <ScrollView style={{ flex: 1 }}>
             <Stack style={{ paddingVertical: 24, paddingHorizontal: 12 }} spacing={24}>
@@ -12,17 +14,29 @@ export default function Settings() {
                         <TableSwitchRow
                             label="Remove blocked messages"
                             value={settings.blocked ?? true}
-                            onValueChange={(v:boolean) => (settings.blocked = v)}
+                            onValueChange={(v: boolean) =>
+                                useHideBlockedAndIgnoredMessagesSettings
+                                    .getState()
+                                    .updateSettings({ blocked: v })
+                            }
                         />
                         <TableSwitchRow
                             label="Remove ignored messages"
                             value={settings.ignored ?? true}
-                            onValueChange={(v:boolean) => (settings.ignored = v)}
+                            onValueChange={(v: boolean) =>
+                                useHideBlockedAndIgnoredMessagesSettings
+                                    .getState()
+                                    .updateSettings({ ignored: v })
+                            }
                         />
                         <TableSwitchRow
                             label="Remove replies to blocked/ignored users"
                             value={settings.removeReplies ?? true}
-                            onValueChange={(v:boolean) => (settings.removeReplies = v)}
+                            onValueChange={(v: boolean) =>
+                                useHideBlockedAndIgnoredMessagesSettings
+                                    .getState()
+                                    .updateSettings({ removeReplies: v })
+                            }
                             subLabel="Filters messages replying to blocked or ignored users."
                         />
                     </TableRowGroup>
