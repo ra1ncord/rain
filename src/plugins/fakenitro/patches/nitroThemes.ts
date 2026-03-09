90fb5696a5a65679bf9e00faa010a1e3a4fc94fd
SerStars <serstars@noreply.codeberg.org>
2026-03-09 23:57:44 +0100

    FakeNitro: Disable 'Sync across clients' for Nitro Themes

--- src/plugins/fakenitro/patches/nitroThemes.ts
+++ src/plugins/fakenitro/patches/nitroThemes.ts
@@ -2,9 +2,12 @@ import { instead } from "@api/patcher";
 import { findByProps } from "@metro";
 
 const canUse = findByProps("canUseClientThemes");
+const AppearanceSettings = findByProps("setShouldSyncAppearanceSettings")
+AppearanceSettings.setShouldSyncAppearanceSettings(false)
 
 export default function getPatches() {
     return [
+        instead("setShouldSyncAppearanceSettings", AppearanceSettings, () => false),
         instead("canUseClientThemes", canUse, () => true),
     ];
 }
