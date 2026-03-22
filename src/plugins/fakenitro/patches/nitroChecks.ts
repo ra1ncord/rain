922839decafc8d2dd7338cf958ea206becee8d54
j <joamonca@noreply.codeberg.org>
2026-03-22 14:53:25 +0100

    feat(fakenitro): no longer able to react with custom emojis (better ux as it never worked), todo make the god damn locks appear

--- src/plugins/fakenitro/patches/nitroChecks.ts
+++ src/plugins/fakenitro/patches/nitroChecks.ts
@@ -1,7 +1,8 @@
-import { instead } from "@api/patcher";
+import { after, instead } from "@api/patcher";
 import { findByProps, findByStoreName } from "@metro";
 
 const nitroInfo = findByProps("canUseEmojisEverywhere");
+const emojiUtils = findByProps("getEmojiUnavailableReason");
 const { getCurrentUser } = findByStoreName("UserStore");
 
 export default function getPatches() {
@@ -9,23 +10,44 @@ export default function getPatches() {
         instead("canUseEmojisEverywhere", nitroInfo, (args, orig) => {
             if (getCurrentUser?.().premiumType !== null)
                 return orig(...args);
-
             return true;
         }),
         instead("canUseAnimatedEmojis", nitroInfo, (args, orig) => {
             if (getCurrentUser?.().premiumType !== null)
                 return orig(...args);
-
             return true;
         }),
 
-        // patch credits: https://github.com/aliernfrog/vd-plugins/blob/master/plugins/FreeStickers/src/patches/nitro.ts
+        // blocks reactions from the big modal
+        after("getEmojiUnavailableReason", emojiUtils, (args, result) => {
+            if (args[0]?.intention === 0 && result === null && getCurrentUser?.().premiumType === null) {
+                const { emoji, guildId, channel } = args[0];
+                console.log(emoji);
+                const currentGuildId = guildId ?? channel?.getGuildId?.();
+                if (emoji.guildId !== currentGuildId || emoji.animated) {
+                    return 0; // DISALLOW_EXTERNAL
+                }
+            }
+            return result;
+        }),
+
+        // blocks reactions from the quick selector
+        after("isEmojiPremiumLocked", emojiUtils, (args, result) => {
+            if (args[0]?.intention === 0 && !result && getCurrentUser?.().premiumType === null) {
+                const { emoji, guildId, channel } = args[0];
+                const currentGuildId = guildId ?? channel?.getGuildId?.();
+                if (emoji.guildId !== currentGuildId || emoji.animated) {
+                    return true;
+                }
+            }
+            return result;
+        }),
+
+        // Stickers
         instead(nitroInfo.canUseCustomStickersEverywhere ? "canUseCustomStickersEverywhere" : "canUseStickersEverywhere", nitroInfo, (args, orig) => {
             if (getCurrentUser?.().premiumType !== null)
                 return orig(...args);
-
             return true;
         }),
-
     ];
 }
