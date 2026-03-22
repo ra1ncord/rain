915b6a7e880a824d1b86e775f3e58c4a4db0e40b
j <joamonca@noreply.codeberg.org>
2026-03-22 16:40:36 +0100

    refactor(fakenitro):reduce code redundancy

--- src/plugins/fakenitro/patches/nitroChecks.ts
+++ src/plugins/fakenitro/patches/nitroChecks.ts
@@ -5,50 +5,47 @@ const nitroInfo = findByProps("canUseEmojisEverywhere");
 const emojiUtils = findByProps("getEmojiUnavailableReason");
 const { getCurrentUser } = findByStoreName("UserStore");
 
+
+function patchReaction(args: any[], result: any, response: any) {
+    if (args[0]?.intention === 0 && result === null && getCurrentUser?.().premiumType === null) {
+        const { emoji, guildId, channel } = args[0];
+        if (emoji.type === 0) return result; // type 0 is twemoji
+        const currentGuildId = guildId ?? channel?.getGuildId?.();
+        if (emoji.guildId !== currentGuildId || emoji.animated) {
+            return response;
+        }
+    }
+    return result;
+}
+
+function patchNitro(orig: Function, args: any[]) {
+    if (getCurrentUser?.().premiumType !== null)
+        return orig(...args);
+    return true;
+}
+
 export default function getPatches() {
     return [
         instead("canUseEmojisEverywhere", nitroInfo, (args, orig) => {
-            if (getCurrentUser?.().premiumType !== null)
-                return orig(...args);
-            return true;
+            return patchNitro(orig, args);
         }),
         instead("canUseAnimatedEmojis", nitroInfo, (args, orig) => {
-            if (getCurrentUser?.().premiumType !== null)
-                return orig(...args);
-            return true;
+            return patchNitro(orig, args);
         }),
 
         // blocks reactions from the big modal
         after("getEmojiUnavailableReason", emojiUtils, (args, result) => {
-            if (args[0]?.intention === 0 && result === null && getCurrentUser?.().premiumType === null) {
-                const { emoji, guildId, channel } = args[0];
-                if (emoji.type === 0) return result; // type 0 is twemoji
-                const currentGuildId = guildId ?? channel?.getGuildId?.();
-                if (emoji.guildId !== currentGuildId || emoji.animated) {
-                    return 0; // DISALLOW_EXTERNAL
-                }
-            }
-            return result;
+            return patchReaction(args, result, 0); // 0 = DISALLOW_EXTERNAL
         }),
 
         // blocks reactions from the quick selector
         after("isEmojiPremiumLocked", emojiUtils, (args, result) => {
-            if (args[0]?.intention === 0 && !result && getCurrentUser?.().premiumType === null) {
-                const { emoji, guildId, channel } = args[0];
-                if (emoji.type === 0) return result; // type 0 is twemoji
-                const currentGuildId = guildId ?? channel?.getGuildId?.();
-                if (emoji.guildId !== currentGuildId || emoji.animated) {
-                    return true;
-                }
-            }
-            return result;
+            patchReaction(args, result, true);
         }),
 
         // sticker patch credits: https://github.com/aliernfrog/vd-plugins/blob/master/plugins/FreeStickers/src/patches/nitro.ts
         instead(nitroInfo.canUseCustomStickersEverywhere ? "canUseCustomStickersEverywhere" : "canUseStickersEverywhere", nitroInfo, (args, orig) => {
-            if (getCurrentUser?.().premiumType !== null)
-                return orig(...args);
-            return true;
+            return patchNitro(orig, args);
         }),
     ];
 }
