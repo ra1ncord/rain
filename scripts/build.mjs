34d165bee00932874ee53fd217a6d6d6f780c736
cocobo1 <CocoboTheOne@gmail.com>
2026-03-23 17:51:49 +0200

    feat: add version check

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -79,7 +79,9 @@ const config = {
                                 constModules: {
                                     globals: {
                                         "rain-build-info": {
-                                            version: `"v0.9.0"`
+                                            version: `"v0.9.0"`,
+                                            // idk why ios uses funny version names
+                                            supportedVersions: '["320012", "96094"]'
                                         }
                                     }
                                 },
