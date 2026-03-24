c2f442f680559013818be7fd300037b3df2111cb
cocobo1 <CocoboTheOne@gmail.com>
2026-03-24 19:35:07 +0200

    fix: theme wallpaper crash

--- src/plugins/_core/painter/themes/patches/storage.ts
+++ src/plugins/_core/painter/themes/patches/storage.ts
@@ -22,16 +22,19 @@ export default function patchStorage() {
             if (state) state.theme = _colorRef.key;
         }),
         before("set", mmkvStorage, ([key, value]) => {
-            if (!patchedKeys.has(key)) return;
+            if (!patchedKeys.has(key) || !value) return;
+        
+            try {
+                if (value._state && value._state.theme) {
+                    const lastTheme = _colorRef.lastSetDiscordTheme || "darker";
+                    if (value._state.theme.startsWith("rain-theme-")) {
+                        value._state.theme = lastTheme;
+                    }
+                }
+            } catch (err) {
+            }
 
-            const json = JSON.stringify(value);
-            const lastSetDiscordTheme = _colorRef.lastSetDiscordTheme ?? "darker";
-            const replaced = json.replace(
-                /"theme":"rain-theme-\d+"/,
-                `"theme":${JSON.stringify(lastSetDiscordTheme)}`
-            );
-
-            return [key, JSON.parse(replaced)];
+            return [key, value];
         })
     ];
 
