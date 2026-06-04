98c0e72b517f4256d93e21f68c537f4c917fc0fd
SerStars <serstars@noreply.codeberg.org>
2026-06-04 14:11:10 +0200

    feat(settings): Make settings not crash on older versions.

--- src/plugins/_core/settings/patches/tabs.tsx
+++ src/plugins/_core/settings/patches/tabs.tsx
@@ -1,11 +1,14 @@
 import { after } from "@api/patcher";
 import { useSettings } from "@api/settings";
+import { NativeClientInfoModule } from "@api/native/modules";
 import { TableRow } from "@metro/common/components";
 import { findByPropsLazy } from "@metro/wrappers";
 import React from "react";
 
 import { registeredSections } from "..";
 import { CustomPageRenderer, wrapOnPress } from "./shared";
+
+const isNewDiscord = Number(NativeClientInfoModule.getConstants().Version) > 329;
 
 const settingConstants = findByPropsLazy("SETTING_RENDERER_CONFIG");
 const createListModule = findByPropsLazy("createList");
@@ -14,13 +17,12 @@ export function patchTabsUI(unpatches: (() => void | boolean)[]) {
     const getRows = () => Object.values(registeredSections)
         .flatMap(sect => sect.map(row => ({
             [row.key]: {
-                title: row.title,
+                ...(isNewDiscord ? { useTitle: row.title } : { title: row.title }),
                 type: "pressable",
                 icon: row.icon,
                 IconComponent: () => <TableRow.Icon source={row.icon} />,
                 usePredicate: row.usePredicate,
                 useTrailing: row.useTrailing,
-                // useTitle: row.title,
                 onPress: wrapOnPress(row.onPress, null, row.render, row.title()),
                 withArrow: true,
                 ...row.rawTabsConfig
@@ -38,8 +40,7 @@ export function patchTabsUI(unpatches: (() => void | boolean)[]) {
             ...rendererConfigValue,
             RAIN_CUSTOM_PAGE: {
                 type: "route",
-                title: "Rain",
-                // useTitle: () => "Rain",
+                ...(isNewDiscord ? { useTitle: () => "Rain" } : { title: "Rain" }),
                 screen: {
                     route: "RAIN_CUSTOM_PAGE",
                     getComponent: () => CustomPageRenderer
