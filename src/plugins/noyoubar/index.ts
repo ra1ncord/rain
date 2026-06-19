b94c4766cc200b28c6b357f266efb7096f857f92
cocobo1 <CocoboTheOne@gmail.com>
2026-06-19 18:46:41 +0000

    feat: noyoubar plugin

--- src/plugins/noyoubar/index.ts
+++ src/plugins/noyoubar/index.ts
@@ -8,10 +8,10 @@ const EXPERIMENT_ID = "2026-01-you-bar";
 // this plugin is also kinda a demo on how to override apex experiments :P
 
 export default definePlugin({
-    name: "YouBar",
-    description: "Enables YouBar via experiment override",
+    name: "NoYouBar",
+    description: "Disables YouBar via experiment override",
     author: [Developers.cocobo1],
-    id: "youbar",
+    id: "noyoubar",
     version: "1.0.0",
 
     // since this is a demo i should probably mention this isnt needed for all experiment overrides
@@ -19,7 +19,7 @@ export default definePlugin({
 
     start() {
         // the number can be changed to change the treatment
-        ExperimentManager.overrideBucket("apex", EXPERIMENT_ID, 1);
+        ExperimentManager.overrideBucket("apex", EXPERIMENT_ID, 0);
     },
     stop() {
         ExperimentManager.overrideBucket("apex", EXPERIMENT_ID, null);
